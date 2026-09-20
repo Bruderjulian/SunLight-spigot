b4c6aed68311f6543a6968bcb1c34864fffee455
BruderJulian <julian@familie-zech.de>
2026-09-20 16:54:08 +0200

    removed Recipe Module

--- Core/src/main/java/su/nightexpress/sunlight/moduleImpl/recipes/RecipesFiles.java
+++
@@ -1,8 +0,0 @@
-package su.nightexpress.sunlight.moduleImpl.recipes;
-
-public class RecipesFiles {
-
-    public static final String DIR_RECIPES = "/recipes/";
-
-    public static final String FILE_EXAMPLE = "example_recipe";
-}
